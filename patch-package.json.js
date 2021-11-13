35f37102b3ab8d3886e57e603d193db7db425038
Meno Abels <meno.abels@adviser.com>
2021-11-13 11:19:17 +0100

    chore: test patch package.json

--- patch-package.json.js
+++ patch-package.json.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 let version = process.argv[process.argv.length-1]
 version = version.split('/').slice(-1)[0].replace(/^v/, '')
 console.error(`Patch package.json version to ${version}`)
-const packageJson = JSON.parse(fs.readFileSync('package.json'))
+const packageJson = JSON.parse(fs.readFileSync('package.json').toString())
 packageJson.version = version
-console.log(JSON.stringify(packageJson, undefined, 2))
+fs.writeFileSync('package.json', JSON.stringify(packageJson, undefined, 2) + "\n")
 
